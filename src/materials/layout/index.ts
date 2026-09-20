3387e262913c9974b38b6aa0ba39706c97f813a8
Tomz Dang <dangjingtao@163.com>
2026-09-20 21:42:48 +0800

    feat: export builtin layout surface

---
+++ src/materials/layout/index.ts
@@ -0,0 +1,9 @@
+export * from "./builtin-layout-renderer.js";
+export * from "./font.js";
+export * from "./safe-rich-layout.js";
+export {
+  builtinLayoutFontFamily,
+  compileSafeRichLayoutToTakumi,
+  SafeLayoutResourceError,
+  type SafeLayoutResources,
+} from "./takumi-compiler.js";
